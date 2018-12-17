e7fcabb4e0a355ce59b16c066a13c48bcb3ab26e
CastleMY <490523604@qq.com>
2018-12-17 17:40:27 +0800

    v1.0.3

--- src/use/config.ts
+++ src/use/config.ts
@@ -1,9 +1,24 @@
 import { Context } from "koa";
 import { env } from 'process'
 import { response } from "../utils";
-import { resolve } from "path";
+import { resolve, join } from 'path';
+import { exists } from "mz/fs";
 export async function config(ctx: Context, next: Function) {
-    let ConfigFile = env.CONFIG_FILE || 'castle-config'
+    let ConfigFile = env.CONFIG_FILE;
+    if (!ConfigFile) {
+        let file = [
+            resolve(`dist/config/${env.NODE_ENV}.js`),
+            resolve(`dist/config/index.js`),
+        ];
+        for (let i = 0; i < file.length; i++) {
+            if (await exists(file[i])) {
+                ConfigFile = file[i]
+            }
+        }
+    }
+    if (!ConfigFile) {
+        ConfigFile = 'castle-config'
+    }
     if (['.', '/'].indexOf(ConfigFile.substr(0, 1)) > -1) {
         ConfigFile = resolve(ConfigFile);
     }
