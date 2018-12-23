1b37a731c6df923992bb369a6e0d804859cda9e4
CastleMY <490523604@qq.com>
2018-12-23 22:55:33 +0800

    cors and package types

--- src/use/cors.ts
+++ src/use/cors.ts
@@ -7,7 +7,7 @@ export default async function cors(ctx: Context, next: Function) {
 }
 function corsset(ctx: Context) {
     ctx.set('Access-Control-Allow-Origin', ctx.header.origin);
-    ctx.set("Access-Control-Allow-Headers", "x-requested-with, accept, origin, content-type");
+    ctx.set("Access-Control-Allow-Headers", "x-requested-with, accept, origin, content-type, token");
     ctx.set("Access-Control-Allow-Credentials", 'true');
     ctx.set("Access-Control-Max-Age", '86400');
 }
