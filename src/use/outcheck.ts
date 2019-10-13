f930a635c0e212e80079715ad34d1f5a4e061e7e
castleiMac <castle@tansuyun.cn>
2019-10-13 23:19:21 +0800

    server`s outcheck hook

--- src/use/outcheck.ts
+++ src/use/outcheck.ts
@@ -1,9 +1,13 @@
 import { Context } from 'koa';
+import Hook, { HookWhen } from '@ctsy/hook';
 export default async function outcheck(ctx: Context, next: Function) {
     try {
+        await Hook.emit('outcheck', HookWhen.Before, ctx, {})
         await next()
+        await Hook.emit('outcheck', HookWhen.After, ctx, {})
     } catch (error) {
         ctx.error = error;
+        await Hook.emit('outcheck', HookWhen.Error, ctx, error)
     } finally {
         if (ctx.config && !ctx.config.sendFile) {
             ctx.body = ctx.config && ctx.config.outcheck ? await ctx.config.outcheck(ctx) : { c: ctx.status, d: ctx.body, i: '', e: 'Not Found' };
