3daa48248120cd13afd8f45f9ec6440c13d62825
castleiMac <castle@tansuyun.cn>
2020-06-18 16:38:09 +0800

    add bodyLimit for parse

--- src/use/parse.ts
+++ src/use/parse.ts
@@ -2,5 +2,7 @@ const bodyparser: any = require('koa-bodyparser')
 const multiparty: any = require('koa2-multiparty')
 export const multi = multiparty()
 export const body = bodyparser({
-    enableTypes: ['json', 'form', 'text']
+    enableTypes: ['json', 'form', 'text'],
+    jsonLimit: '30mb',
+    formLimit: '30mb',
 })
