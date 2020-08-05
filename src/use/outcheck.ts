2023ea4095696d38b2a1740374f78e4900faf8c0
castleiMac <castle@tansuyun.cn>
2020-08-05 17:39:33 +0800

    恢复输出数据的处理过程

--- src/use/outcheck.ts
+++ src/use/outcheck.ts
@@ -11,11 +11,11 @@ export default async function outcheck(ctx: Context, next: Function) {
         await Hook.emit(ServerHook.Outcheck, HookWhen.Error, ctx, error)
     } finally {
         if (ctx.config && !ctx.config.sendFile) {
-            if (ctx.body instanceof ReadableStream) {
+            // if (ctx.body instanceof ReadableStream) {
 
-            } else {
-                ctx.body = ctx.config && ctx.config.outcheck ? await ctx.config.outcheck(ctx) : { c: ctx.status, d: ctx.body, i: '', e: 'Not Found' };
-            }
+            // } else {
+            ctx.body = ctx.config && ctx.config.outcheck ? await ctx.config.outcheck(ctx) : { c: ctx.status, d: ctx.body, i: '', e: 'Not Found' };
+            // }
         }
     }
 }
